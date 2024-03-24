1c584325635fe559410837d1a19f19cccb42abba
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-03-24 04:32:59 -0400

    added view logic for student details and configured urls

---
+++ drfAPI/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path, re_path
+from . import views
+
+app_name = 'drfAPI'
+
+urlpatterns = [
+    path('students/',views.list_students, name='students'),
+    path('student/<int:pk>/',views.student_detail,'student_detail')
+]
--- drfAPI/views.py
+++ drfAPI/views.py
@@ -3,8 +3,10 @@ from .serializers import StudentSerializer
 from .models import Student
 from rest_framework.response import Resopnse
 from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET','POST'])
 def list_students(request):
     if request.method == 'GET':
         students = Student.models.all()
@@ -18,4 +20,24 @@ def list_students(request):
         
         return Response(serialzer.errors,status = status.HTTP_400_BAD_REQUEST)
 
+def student_detail(request,pk):
+    try:
+        student = Student.objects.get(pk)
+    except StudentNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Resopnse(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student,data = request.data)
+        if serilizer.is_valid():
+            serializer.save()
+            return Resopnse(serializer.data)
+        return Resopnse(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        student.delete()
+        return Resopnse(status = status.HTTP_204_NO_CONTENT)
         
