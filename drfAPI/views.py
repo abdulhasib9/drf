83f533fa5553878cda365afe41b338c01621a756
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-03-24 03:36:22 -0400

    added urls class and firts function-based view for retriving the students list

--- drfAPI/views.py
+++ drfAPI/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework.response import Resopnse
 # Create your views here.
+def list_students(request):
+    if request.method == 'GET':
+        students = Student.models.all()
+        serilizers = StudentSerializer(students,many=True)
+        return Resopnse(serilizers,)
+        
