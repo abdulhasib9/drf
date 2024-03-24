5423ac2c001930543f097d2e979c9d29221bbf26
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-03-24 04:12:34 -0400

    added code example for student creations

--- drfAPI/views.py
+++ drfAPI/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from .serializers import StudentSerializer
 from .models import Student
 from rest_framework.response import Resopnse
+from rest_framework import status
+
 # Create your views here.
 def list_students(request):
     if request.method == 'GET':
         students = Student.models.all()
         serilizers = StudentSerializer(students,many=True)
-        return Resopnse(serilizers,)
+        return Resopnse(serilizers.data,status = status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serialzer = StudentSerilizers(data = request.data)
+        if serilizer.is_valid():
+            serialzer.save()
+            return Resopnse(serialzer.data,status = status.HTTP_201_CREATED)
+        
+        return Response(serialzer.errors,status = status.HTTP_400_BAD_REQUEST)
+
         
